8bfd6b58d3ebb35fddc2583ff723cd329d59871c
Mabelys Arias <MabelysA@gmail.com>
2021-06-23 15:05:38 -0400

    creates routes and some controllers

--- app/controllers/validations_controller.rb
+++ app/controllers/validations_controller.rb
@@ -1,2 +1,20 @@
 class ValidationsController < ApplicationController
+  def index
+    examples = Example.all
+    render json: examples.as_json
+  end
+
+  def create
+    examples = Example.new(
+      first_name: params["first_name"],
+      last_name: params["last_name"],
+      email: params["email"],
+    )
+    examples.save
+    render json: examples.as_json
+  end
+
+  def show
+    examples_id = params[:if]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get "/examples" => "validations#index"
+  post "/examples/" => "validations#create"
+  get "/examples/:id" => "validations#show"
+  patch "/examples/:id" => "validations#update"
+  delete "/examples/:id" => "validations#destroy"
 end
